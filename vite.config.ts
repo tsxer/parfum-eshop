aadf4c328e482569045e96c385f0225489c92187
Damir <dmrkudinov@gmail.com>
2026-01-17 22:43:36 +0100

    Remove css config to fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,5 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/styles/variables" as v;`
-      }
-    }
   }
 });
